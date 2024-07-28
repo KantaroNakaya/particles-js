288812a7f0ebd400bc7188c2eaccc0eb14b19156
KantaroNakaya <shimeno38saiko@gmail.com>
2024-07-28 20:53:03 +0900

    サイト構成の変更

--- common/js/changeBg.js
+++ common/js/changeBg.js
@@ -9,7 +9,7 @@ document.getElementById('change-bg-button02').addEventListener('click', function
     document.body.style.opacity = '1.0';
 });
 document.getElementById('change-bg-button03').addEventListener('click', function() {
-    document.body.style.backgroundImage = 'url("common/img/bg_01.png")';
+    document.body.style.backgroundImage = 'url("../common/img/bg_01.png")';
     document.body.style.backgroundColor = 'rgba(255,255,255,0.8)';
     document.body.style.backgroundBlendMode = 'lighten';
 });
